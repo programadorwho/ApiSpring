c698f6378ecf129192b3d15c9f4e0f55f3f2786c
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2019-10-24 13:59:47 -0300

    initial commit
    Create a Spring boot project

---
+++ src/test/java/programadorwho/api/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package programadorwho.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
